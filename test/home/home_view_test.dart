a21701ce22a2ce04bccc1a0ec83873defd64e347
jacob blankenship <jacob.blankenship@triceimaging.com>
2023-07-21 18:32:26 -0400

    test

--- test/home/home_view_test.dart
+++ test/home/home_view_test.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/form.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,7 +9,16 @@ import 'package:sofi_interview/app/feature/home/views/home.dart';
 
 class MockHomeController extends GetxController
     with Mock
-    implements HomeViewController {}
+    implements HomeViewController {
+  @override
+  GlobalKey<FormState> get formKey => GlobalKey<FormState>();
+  @override
+  TextEditingController get textEditingController => TextEditingController();
+  @override
+  int get index => 0.obs.value;
+  @override
+  PageState get pageState => PageState.init.obs.value;
+}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
